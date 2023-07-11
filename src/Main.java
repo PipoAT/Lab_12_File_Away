37b7aca927619727b32cd3899cdf2e05276a971d
Andrew Pipo <atpipo2026@gmail.com>
2023-07-11 10:45:58 -0400

    Completed Lab 12 with testing

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,64 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static java.nio.file.StandardOpenOption.CREATE;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        // initialize the JFileChooser to allow user to select .txt file
+        JFileChooser fileSelection = new JFileChooser();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // establish filters to select .txt files ONLY
+        fileSelection.setAcceptAllFileFilterUsed( false );
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        fileSelection.setFileFilter( new FileNameExtensionFilter( "Text Files (.txt)", "txt" ) );
+
+        File selectedFile;
+        String lineString;
+
+        // set the initial directory that JFileChooser opens up to as the current directory
+        File dir = new File( System.getProperty( "user.dir" ) );
+        fileSelection.setCurrentDirectory( dir );
+
+        // if user selects a file and hits okay/select (successful attempt)
+        if (fileSelection.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION) {
+            // takes the selected file and gets the path, opens file
+            selectedFile = fileSelection.getSelectedFile();
+            Path path = selectedFile.toPath();
+            InputStream in = new BufferedInputStream( Files.newInputStream( path, CREATE ) );
+            BufferedReader textFileReader = new BufferedReader( new InputStreamReader( in ) );
+
+            // initialize variables to track the official counts of the lines, words, characters in a specified file
+            int linesInDocumentCount = 0;
+            int wordsInDocumentCount = 0;
+            int charactersInDocumentCount = 0;
+
+            // continues to loop and read the document until there is no more text to read
+            while (textFileReader.ready()) {
+                lineString = textFileReader.readLine(); // reads the next line
+                String[] wordsArray = lineString.split( " " ); // initializes the array to keep track of the words for each line
+                wordsInDocumentCount = wordsInDocumentCount + wordsArray.length; // tracks the word count
+                charactersInDocumentCount = charactersInDocumentCount + lineString.length(); // tracks the character count
+                linesInDocumentCount++; // adds one for each line that is read
+            }
+
+            // print out the file alongside the count of lines, words, and characters in file.
+            System.out.println("The file that was selected was: " + selectedFile.getName());
+            System.out.println( "The number of lines contained in the file is: " + linesInDocumentCount );
+            System.out.println( "The number of words contained in the file is: " + wordsInDocumentCount);
+            System.out.println( "The total number of characters in the file is: " + charactersInDocumentCount );
+
+        } else {
+            // if user does not select file, display file was not selected
+            System.out.println("No file was selected.");
         }
     }
 }
